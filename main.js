ff2fc03291c41c218b7f5a6ce64a800aabf231ec
Subeen Chun <petitloutre@naver.com>
2024-09-11 12:20:25 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+let hamburger = document.querySelector(".hamburger");
+hamburger.onclick = function () {
+    navBar = document.querySelector(".nav_bar");
+    navBar.classList.toggle("active");
+
+}
